21578442a4250e4868351cfccdaa1c4000eb5a0b
Eldhose2006 <eldhoseabrahamemerson2028@cs.sjcetpalai.ac.in>
2024-11-20 23:26:24 +0530

    Update question4.py

--- Day1/Eldhose/question4.py
+++ Day1/Eldhose/question4.py
@@ -1,1 +1,11 @@
-print
+total_chocolates = int(input("Enter the total number of chocolates: "))
+total_people = int(input("Enter the number of people: "))
+
+if total_people == 0:
+    print("Cannot divide chocolates among zero people!")
+else:
+    chocolates_per_person = total_chocolates // total_people
+    remaining_chocolates = total_chocolates % total_people
+
+    print(f"Each person gets {chocolates_per_person} chocolates.")
+    print(f"Remaining chocolates: {remaining_chocolates}")
