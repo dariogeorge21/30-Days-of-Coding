0fa3349d7be8e8603166d99eac6833c1d65336d6
Amal Roy <amalroy2028@cs.sjcetpalai.ac.in>
2024-11-20 21:04:56 +0530

    Update Amal

---
+++ Day1/Amal/no1.py
@@ -0,0 +1,17 @@
+a=int(input("Enter the first number:"))
+
+b=int(input("Enter the second number:"))
+
+d=(a+b)
+  
+print("The sum is=",d)
+
+    𝗼𝘂𝘁𝗽𝘂𝘁
+
+Enter the first number:5
+
+Enter the second number:7
+
+The sum is= 12
+
+[Program finished]
