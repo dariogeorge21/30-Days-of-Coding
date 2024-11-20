0f62c659b9314968fce71a1ea0b468ad64d6a615
Adith John <adithjohn2028@cs.sjcetpalai.ac.in>
2024-11-20 20:23:34 +0530

    Update day1_Q3.py

--- Day1/Adith/day1_Q3.py
+++ Day1/Adith/day1_Q3.py
@@ -1,1 +1,19 @@
+n=int(input("Enter the day:"))
 
+match n:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday") 
+    case 5:
+        print("friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _:
+        print("Invalid")
