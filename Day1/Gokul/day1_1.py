b4ed4d79527ae21b7b494d5a4d41767be793c858
GokulShaji2005 <gokulshaji2005@gmail.com>
2024-11-20 19:32:57 +0530

    Add files via upload

---
+++ Day1/Gokul/day1_1.py
@@ -0,0 +1,5 @@
+#Question 1 : Write a program to find sum of 2 numbers?
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+
+print(f"The sum of {a} and {b} = {a+b}")
