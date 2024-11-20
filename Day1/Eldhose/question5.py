6e3d732ac71da86447ac31f5075d457a1150ea15
Eldhose2006 <eldhoseabrahamemerson2028@cs.sjcetpalai.ac.in>
2024-11-20 23:28:05 +0530

    Update question5.py

--- Day1/Eldhose/question5.py
+++ Day1/Eldhose/question5.py
@@ -1,1 +1,8 @@
-e
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+smallest = min(num1, num2, num3)
+square = smallest ** 2
+
+print(f"The smallest number is {smallest}, and its square is {square}.")
