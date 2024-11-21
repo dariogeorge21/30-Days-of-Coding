3c88663f042a341829fc97a5cdf9407debaa7ed2
Amal Roy <amalroy2028@cs.sjcetpalai.ac.in>
2024-11-21 18:18:26 +0530

    Update Amal

---
+++ Day1/Amal/Day 1,Qn. 2.py
@@ -0,0 +1,18 @@
+#Question 2 : Write a program to find wheather the number is odd or even? 
+
+
+Number = int(input("Enter a number: "))
+
+if Number%2==0:
+               print(Number," is even no")
+
+else:
+       print(Number," is odd no")
+          
+
+      𝗢𝗨𝗧𝗣𝗨𝗧
+
+Enter a number: 7
+7  is odd no
+
+[Program finished]
