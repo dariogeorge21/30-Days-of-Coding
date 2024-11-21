c2a8e7c4ebec64eea995b23f325b3980c5387736
GokulShaji2005 <gokulshaji2005@gmail.com>
2024-11-21 20:05:11 +0530

    Add files via upload

---
+++ Day2/Gokul/day2_1.py
@@ -0,0 +1,14 @@
+#Question 1: Write a program to check 2 variables have same value or not using Identity Operator.
+
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+
+if a is b:
+    print("a and b have same value")
+else:
+    print("Not same")
+
+#Enter the first number:3
+#Enter the second number:3
+#a and b have same value
+
