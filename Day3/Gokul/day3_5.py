fb878f3006b413eed8015a4f50730fd05a8a716d
GokulShaji2005 <gokulshaji2005@gmail.com>
2024-11-22 20:48:26 +0530

    Add files via upload

---
+++ Day3/Gokul/day3_5.py
@@ -0,0 +1,22 @@
+n=int(input("Enter the number:"))
+
+if n>0 and n%2==0:
+    print(f"{n} is positive and even")
+    
+elif n>0 and n%2!=0:
+    print(f"{n} is positive and odd")
+    
+elif n<0 and n%2==0:
+    print(f"{n} is negative and even")
+    
+elif n<0 and n%2!=0:
+    print(f"{n} is negative  and odd")
+
+else:
+    print("Zero")
+
+#Enter the number:-3
+#-3 is negative  and odd
+
+
+    
