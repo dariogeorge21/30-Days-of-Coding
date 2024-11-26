3cf328ae7577a5fb2d2bfffb5aaa5f61210b74da
GokulShaji2005 <gokulshaji2005@gmail.com>
2024-11-26 08:03:09 +0530

    Create Day7_2.py

---
+++ Day7/Gokul/Day7_2.py
@@ -0,0 +1,8 @@
+Write a program to print Triangular pattern using a for loop
+Pattern :
+A
+A B
+A B C
+A B C D
+It must be a right triangular pattern with 4 rows and alphabets increasing in ascending order
+
