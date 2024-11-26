912f63876a9ec45e7622f9def4c82420a920447f
Amal Roy <amalroy2028@cs.sjcetpalai.ac.in>
2024-11-26 16:58:55 +0530

    Update Day3_Qn1.py

--- Day3/Amal/Day3_Qn1.py
+++ Day3/Amal/Day3_Qn1.py
@@ -1,1 +1,31 @@
-Question 1: Write a program that takes a student's marks and prints the grade based on the following criteria:
+#Question 1: Write a program that takes a student's marks and prints the grade based on the following criteria:
+
+#Marks > 90: A
+#Marks > 80: B
+#Marks > 70: C
+#Marks > 60: D
+#Otherwise: F
+
+Mark=int(input("Enter the mark that you got:"))
+
+if Mark>90:
+    print("You have A grade")
+   
+elif Mark>80:
+    print("You have B grade")
+    
+elif Mark>70:
+    print("You have C grade")
+    
+elif Mark>60:
+     print("You have D grade")
+    
+else:
+    print("You have F grade
+
+
+       #𝗢𝗨𝗧𝗣𝗨𝗧
+#Enter the mark that you got:67
+#You have D grade
+
+#[Program finished]
