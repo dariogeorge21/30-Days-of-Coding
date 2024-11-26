343ccafb91018dbf53fad0e9136976e9fb8a25a1
GokulShaji2005 <gokulshaji2005@gmail.com>
2024-11-26 08:06:19 +0530

    Update Day7_2.py

--- Day7/Gokul/Day7_2.py
+++ Day7/Gokul/Day7_2.py
@@ -1,8 +1,19 @@
-Write a program to print Triangular pattern using a for loop
-Pattern :
-A
-A B
-A B C
-A B C D
-It must be a right triangular pattern with 4 rows and alphabets increasing in ascending order
+#Write a program to print Triangular pattern using a for loop
+#Pattern :
+#A
+#A B
+#A B C
+#A B C D
+#It must be a right triangular pattern with 4 rows and alphabets increasing in ascending order
+
+for i in range(5):
+	for j in range(i):
+		print(chr(65+j),end=" ")
+	print()
+
+#output
+#A
+#A B
+#A B C
+#A B C D
 
