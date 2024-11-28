40a91865bcf6953b42a60ad0a0decee46100d2f1
GokulShaji2005 <gokulshaji2005@gmail.com>
2024-11-28 19:00:42 +0530

    Update Day9_3.py

--- Day9/Gokul/Day9_3.py
+++ Day9/Gokul/Day9_3.py
@@ -5,7 +5,7 @@ set2={3,4,5}
 
 print("Union=",set1|set2)# or set1.union(set2)
 print("Intersection=",set1&set2)#or set1.difference(set2)
-print("Symmetric difference=",set1.symmetric_difference(set2))#or set1^set2
+print("difference=",set1.difference(set2))
 
 #Union= {1, 2, 3, 4, 5}
 #Intersection= {3}
