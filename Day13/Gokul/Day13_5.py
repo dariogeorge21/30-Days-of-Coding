f83994c99c6e22f0b4bc65fef8124ffa016660d7
GokulShaji2005 <gokulshaji2005@gmail.com>
2024-12-04 17:27:12 +0530

    Create Day13_5.py

---
+++ Day13/Gokul/Day13_5.py
@@ -0,0 +1,11 @@
+#Problem: Given the dictionary:
+#book = {'title': 'Python Programming', 'author': 'John Doe', 'year': 2021}
+#Write Python code to get and print all the keys and all the values separately.
+
+book = {'title': 'Python Programming', 'author': 'John Doe', 'year': 2021}
+print(book.keys())
+print(book.values())
+
+#output
+#dict_keys(['title', 'author', 'year'])
+#dict_values(['Python Programming', 'John Doe', 2021])
