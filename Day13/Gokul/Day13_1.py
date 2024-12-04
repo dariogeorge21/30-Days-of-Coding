dd32df40b5e8d25815e8ebecda286257e8d9d6fd
GokulShaji2005 <gokulshaji2005@gmail.com>
2024-12-04 16:57:41 +0530

    Create Day13_1.py

---
+++ Day13/Gokul/Day13_1.py
@@ -0,0 +1,15 @@
+#Create a dictionary with the following key-value pairs:
+#'name': 'Alice'
+#'age': 25
+#'city': 'New York'
+#Then, write a Python code to access and print the value of the key 'age'.
+
+
+dict={'name': 'Alice',
+          'age' : 25,
+          'city': 'New York'}
+
+print("The age is",dict["age"])   
+
+#output
+#The age is 25
