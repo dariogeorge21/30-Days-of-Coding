2d895e0f5c80bc9d0eb7ee3bd98dfff91d974448
GokulShaji2005 <gokulshaji2005@gmail.com>
2024-12-04 17:23:15 +0530

    Create Day13_4.py

---
+++ Day13/Gokul/Day13_4.py
@@ -0,0 +1,16 @@
+#Problem: Write a function that checks if the key 'email' exists in the following dictionary:
+
+#user = {'username': 'johndoe', 'email': 'johndoe@example.com'}
+
+#The function should return True if the key exists, otherwise return False
+
+user = {'username': 'johndoe', 'email':        'johndoe@example.com'}
+if "email" in user:
+	print("True")
+else:
+	print("False")
+	
+#output
+#True
+
+
