164441756a38015d5773cde961d60fd72b6f5cc3
GokulShaji2005 <gokulshaji2005@gmail.com>
2024-12-04 17:42:57 +0530

    Create Day14_2.py

---
+++ Day14/Gokul/Day14_2.py
@@ -0,0 +1,12 @@
+#Write a function named square() that takes one number as input and returns the square of that number.
+
+def square(a):
+	  square1=a*a
+	  return square1
+      
+
+num=int(input("Enter the number:"))
+print(f"The square of {num} is" ,square (num))
+
+#Enter the number:5
+#The square of 5 is 25
