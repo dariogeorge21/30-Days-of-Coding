c19c6890b6b1202e7bd671057a26bfc2c10785a2
GokulShaji2005 <gokulshaji2005@gmail.com>
2024-12-04 17:29:47 +0530

    Create Day14_1.py

---
+++ Day14/Gokul/Day14_1.py
@@ -0,0 +1,7 @@
+#Write a Python function named greet() that takes no parameters and prints the message "Hello, World!". Call this function.
+def greet():
+	print("Hello,World!")
+greet()
+
+#output
+#Hello,World!
