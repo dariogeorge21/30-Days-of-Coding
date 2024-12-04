bd8b2e3776c1683598a03a0ff22482a751a2fa8c
GokulShaji2005 <gokulshaji2005@gmail.com>
2024-12-04 17:04:35 +0530

    Create Day13_2.py

---
+++ Day13/Gokul/Day13_2.py
@@ -0,0 +1,11 @@
+#Add a New Key-Value Pair
+#Problem:
+#Given the dictionary: person = {'name': 'Bob', 'age': 30, 'city': 'San Francisco'}
+
+person = {'name': 'Bob', 'age': 30, 
+                 'city': 'San Francisco'}
+person["country"]="USA"
+print(person)
+
+#output
+{'name': 'Bob', 'age': 30, 'city': 'San Francisco', 'country': 'USA'}
