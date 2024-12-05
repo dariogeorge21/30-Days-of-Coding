a4756a61f980223a944b31078b23a54a1733bece
GokulShaji2005 <gokulshaji2005@gmail.com>
2024-12-05 08:42:39 +0530

    Create Day14_4.py

---
+++ Day14/Gokul/Day14_4.py
@@ -0,0 +1,12 @@
+#Write a function named get_min_max() that takes a list of numbers and returns the smallest and largest number from the list.
+
+def get_min_max(temp_list):
+	min_=min(list1)
+	max_=max(list1)
+	return min_,max_
+list1=[3, 1, 8, 2, 7]
+result=get_min_max(list1)
+print(result)
+
+#(1, 8)
+
