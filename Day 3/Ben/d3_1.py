6f8d75b32390819481e0e3dd422d2836506287b7
benjohnyy <benjohnyofficial7@gmail.com>
2024-12-12 22:09:09 +0530

    Create d3_1.py

---
+++ Day 3/Ben/d3_1.py
@@ -0,0 +1,12 @@
+mark=int(input("Enter the number:"))
+
+if mark>=90:
+    print("You have A grade")
+elif mark>=80:
+    print("You have B grade")
+elif mark>=70:
+    print("You have C grade")
+elif mark>=60:
+    print("You have D grade")
+else:
+    print("You have F grade")
