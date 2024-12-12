17222c4a491df21f1eed02f4cb1adbf0ecc50da6
Eldhose2006 <eldhoseabrahamemerson2028@cs.sjcetpalai.ac.in>
2024-12-12 21:19:53 +0530

    Create d3_2.py

---
+++ Day 3/Eldhose/d3_2.py
@@ -0,0 +1,10 @@
+year=int(input("Enter the number:"))
+
+if year%4==0:
+    if year%100!=0 or year%400==0: 
+       print(f"{year} is a leap year")
+       
+    else:print(f"{year} is not a leap year")
+        
+else:
+    print(f"{year} is not a leap year")
