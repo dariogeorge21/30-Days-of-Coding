afa74377853ebc13755cf172c8bee61ed7087a36
benjohnyy <benjohnyofficial7@gmail.com>
2024-12-12 22:01:26 +0530

    d1_4.py

---
+++ Day 1/Ben/d1_4.py
@@ -0,0 +1,6 @@
+n=int(input("Enter the number of people :"))
+c=int(input("Enter the number of choclate :"))
+x=c%n
+y=c//n
+print("Total number of chocalate each person got is :",y)
+print("Chocalate remaining is :",x)
