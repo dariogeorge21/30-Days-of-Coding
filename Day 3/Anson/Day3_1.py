d0006e7257270f2496af8dfde74d62a748e74d67
Anson2006 <ansonjoice2028@cs.sjcetpalai.ac.in>
2024-12-14 16:02:04 +0530

    Update Anson

---
+++ Day 3/Anson/Day3_1.py
@@ -0,0 +1,13 @@
+
+mark=int(input("Enter the number:"))
+
+if mark>=90:
+    print("A grade")
+elif mark>=80:
+    print("B grade")
+elif mark>=70:
+    print(" C grade")
+elif mark>=60:
+    print("D grade")
+else:
+    print( "F grade")
