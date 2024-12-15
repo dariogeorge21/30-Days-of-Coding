948d564649798dfed0e0f72d9a8365759afdf5eb
joanz07 <johanjoseph2028@cs.sjcetpalai.ac.in>
2024-12-15 11:37:43 +0530

    Update Johan

---
+++ Day 3/Johan/Q1.py
@@ -0,0 +1,12 @@
+x=int(input("Enter the number:"))
+
+if x>=90:
+    print(" A grade")
+elif x>=80:
+    print("B grade")
+elif x>=70:
+    print(" C grade")
+elif x>=60:
+    print(" D grade")
+else:
+    print(" F grade")
