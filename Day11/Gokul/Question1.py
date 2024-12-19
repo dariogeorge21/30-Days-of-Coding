809fbdfc8f50cc23beae542a91e6bdcba04e7574
GokulShaji2005 <gokulshaji2005@gmail.com>
2024-12-19 16:51:29 +0530

    Update Question1.py

--- Day11/Gokul/Question1.py
+++ Day11/Gokul/Question1.py
@@ -1,3 +1,6 @@
+#Write a program to check if a given string is a palindrome, ignoring spaces, case, and special characters
+
+
 string="A man, a plan, a canal, Panama!"
 string_without="".join([char for char in string if char.isalnum()])
 string_lower=string_without.lower()
