62dba4d0d0700aa334d5190c42989ea59c43715b
Dario George <edu.dariogeorge21@gmail.com>
2025-01-09 14:28:25 +0530

    Updation

--- Day22/Dario/Question5.py
+++ Day22/Dario/Question5.py
@@ -1,10 +1,4 @@
 def union_of_sets(set1, set2):
-    """
-    Find the union of two sets.
-    :param set1: First set
-    :param set2: Second set
-    :return: A set containing the union of set1 and set2
-    """
     return set1 | set2  
 set_a = {1, 2, 3, 4}
 set_b = {3, 4, 5, 6}
